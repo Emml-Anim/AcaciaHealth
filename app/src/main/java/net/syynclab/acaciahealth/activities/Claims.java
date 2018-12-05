36bb7fb339a4f735d83b17f0e15efcf876028cb9
AE Drew <kojo3anim@gmail.com>
2018-12-05 12:31:07 +0000

    update

--- app/src/main/java/net/syynclab/acaciahealth/activities/Claims.java
+++ app/src/main/java/net/syynclab/acaciahealth/activities/Claims.java
@@ -28,7 +28,7 @@ public class Claims extends AppCompatActivity {
 
         //Initialize Views
         initViews();
-        
+
         claims_upload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
